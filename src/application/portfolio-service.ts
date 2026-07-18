935a97e8f5776725bf058b8350609fc5c3cf9f99
John Viklund <john@viklund.se>
2026-07-17 23:41:35 -0600

    Compose the application portfolio service

---
+++ src/application/portfolio-service.ts
@@ -0,0 +1,32 @@
+import { join } from "node:path";
+
+import { SQLitePortfolioIndex } from "../index/work-item-index";
+import { PortfolioRegistry } from "../workspace/portfolio-registry";
+import { PortfolioService } from "./portfolio";
+
+const applicationRoot = process.cwd();
+const localDataRoot = join(applicationRoot, ".local-data");
+
+export const portfolioRegistryPath = join(localDataRoot, "registry.json");
+export const portfolioIndexPath = join(localDataRoot, "index.sqlite");
+
+let portfolioServicePromise: Promise<PortfolioService> | undefined;
+
+export function getPortfolioService(): Promise<PortfolioService> {
+  if (portfolioServicePromise === undefined) {
+    const registry = new PortfolioRegistry(portfolioRegistryPath);
+    const index = new SQLitePortfolioIndex(portfolioIndexPath);
+    const service = new PortfolioService(registry, index);
+
+    portfolioServicePromise = service
+      .rebuild()
+      .then(() => service)
+      .catch((error: unknown) => {
+        index.close();
+        portfolioServicePromise = undefined;
+        throw error;
+      });
+  }
+
+  return portfolioServicePromise;
+}
